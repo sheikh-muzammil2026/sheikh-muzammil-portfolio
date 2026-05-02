48e222fd1e13cdf13297fbb4744b36d6633dbf53
Sheikh_Muzammil <discordsstudent@gmail.com>
2026-05-02 10:50:25 +0600

    added  Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,16 +11,18 @@ const Experience = () => {
         <div className="border border-gray-700 rounded-2xl p-8">
 
           <h3 className="text-2xl font-semibold mb-2">
-            Frontend Developer Intern
+            MERN Stack Developer (Self-Projects) | 2026 – Present
           </h3>
 
           <p className="text-gray-400 mb-2">
-            ABC Company
+            Self-Employed
           </p>
 
           <p className="text-gray-500 leading-7">
-            Worked on responsive UI design, API integration,
-            and React based frontend applications.
+            
+​Built 3+ responsive projects leveraging Next.js and Tailwind CSS.
+​Experienced in REST API integration and handling dynamic data with MongoDB.
+​Proficient in the entire workflow from UI design to cloud deployment (Vercel/Netlify).
           </p>
         </div>
       </div>
