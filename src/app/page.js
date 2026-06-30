bb12a8872ec09f8ed9e59d1628d413b33a264b36
Sheikh Muzammil <discordsstudent@gmail.com>
2026-07-01 01:50:18 +0600

    Imported projects component in  page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Education from "@/components/Education";
 import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -16,8 +17,10 @@ export default function Home() {
         <Hero />
         <About />
         <Skills />
+    		<Projects/>
         <Education />
         <Experience />
+     		
       </div>
   
     </main>
